d80d65575fc4fa4f81b0f1ab0e0fb89a37fe2bcd
Arran Dacres <arran.dacres@waterstons.com>
2020-02-13 22:31:18 +0000

    Updates

---
+++ precache-manifest.e9d0439b7a742e4a68963e29868a1896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37bdc2fca5b4f4cb15023f029dff9447",
+    "url": "/service-worker-test/index.html"
+  },
+  {
+    "revision": "29740ef3fb114b0b6ee4",
+    "url": "/service-worker-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c1a268d3ae5eef8173e8",
+    "url": "/service-worker-test/static/js/2.2c651fa3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/service-worker-test/static/js/2.2c651fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29740ef3fb114b0b6ee4",
+    "url": "/service-worker-test/static/js/main.f14a1c78.chunk.js"
+  },
+  {
+    "revision": "d4999ca05ac10dda5d21",
+    "url": "/service-worker-test/static/js/runtime-main.6976ba9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/service-worker-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
