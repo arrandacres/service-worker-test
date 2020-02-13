8acacfe92ed88ad317225b4e669f6265f118cf3a
Arran Dacres <arran.dacres@waterstons.com>
2020-02-13 22:39:10 +0000

    Updates

---
+++ precache-manifest.b99dc90b76f0f1f8f6699b15e18f1a14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5155ee4df4d4def08cb6c4d437ea28f9",
+    "url": "/service-worker-test/index.html"
+  },
+  {
+    "revision": "b089fc59c2bf300a3aa1",
+    "url": "/service-worker-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "53ed8d062de45bb73347",
+    "url": "/service-worker-test/static/js/2.9cd23a80.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/service-worker-test/static/js/2.9cd23a80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b089fc59c2bf300a3aa1",
+    "url": "/service-worker-test/static/js/main.f21853ce.chunk.js"
+  },
+  {
+    "revision": "d4999ca05ac10dda5d21",
+    "url": "/service-worker-test/static/js/runtime-main.6976ba9d.js"
+  }
+]);
